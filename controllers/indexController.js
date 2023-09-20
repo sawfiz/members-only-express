98030b08afb4098faa2182a66dbc88c8ac2c2bd3
sawfiz <dashu.up@gmail.com>
2023-09-19 20:48:27 -0400

    Validate and sanitize sign up

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
-const News = require('../models/post');
+const { body, validationResult } = require('express-validator');
+
+const Post = require('../models/post');
 const User = require('../models/user');
 
 // const passport = require('passport');
@@ -10,7 +12,7 @@ exports.messages_list_get = asyncHandler(async (req, res, next) => {
   // Dispaly all news, both published and unpublished
   // const allMessages = await this.messages_list_get.find({ publish: true }).sort({ date: -1 }).exec();
   // res.render('news_list', { news_list: allNews, admin: false });
-  res.render( "index", {title: "Welcome to the home page!"})
+  res.render('index', { title: 'Welcome to the home page!' });
 });
 
 // Display login on GET
@@ -35,35 +37,77 @@ exports.signup_get = (req, res, next) => {
 };
 
 // Handle signup on POST
-exports.signup_post = async (req, res, next) => {
-  console.log(req.body);
-  if (req.body.password === req.body.password1) {
-    console.log("Password match");
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      // if err, do something
-      if (err) {
-        console.log('Error hashing password');
-      } // otherwise, store hashedPassword in DB
-      else {
-        try {
-          const user = new User({
-            first_name: req.body.firstName,
-            last_name: req.body.lastName,
-            username: req.body.username,
-            password: hashedPassword,
-          });
-          console.log("🚀 ~ file: indexController.js:54 ~ bcrypt.hash ~ user:", user)
-          const result = await user.save();
-          res.redirect('/');
-        } catch (err) {
-          return next(err);
+exports.signup_post = [
+  // Validate and sanizize fields
+  body('firstName', 'First name must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('lastName', 'Last name must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('username', 'Username must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty() || req.body.password !== req.body.password1) {
+      const user = new User({
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        username: req.body.username,
+        password: req.body.password,
+      });
+
+      const error =
+        req.body.password === req.body.password1
+          ? ''
+          : 'Passwords do not match.';
+
+      res.render('signup', {
+        title: 'Signup',
+        user,
+        error,
+        errors: errors.array(),
+      });
+    } else {
+      console.log('Password match');
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        // if err, do something
+        if (err) {
+          console.log('Error hashing password');
+        } // otherwise, store hashedPassword in DB
+        else {
+          try {
+            const user = new User({
+              first_name: req.body.firstName,
+              last_name: req.body.lastName,
+              username: req.body.username,
+              password: hashedPassword,
+            });
+
+            if (!errors.isEmpty()) {
+              res.render('signup', {
+                title: 'Signup',
+                user,
+                erros: errors.array(),
+              });
+            } else {
+              await user.save();
+              res.redirect('/');
+            }
+          } catch (err) {
+            return next(err);
+          }
         }
-      }
-    });
-  } else {
-    res.render('signup', {title:"Sign Up", error: "Password do not match."})
-  }
-};
+      });
+    }
+  },
+];
 
 // // Display logout page on GET
 // exports.logout_get = async (req, res, next) => {
